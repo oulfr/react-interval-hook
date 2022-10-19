418f22c165f1a6995055e505dd24e1ae33e571ba
A.O <35069360+oulfr@users.noreply.github.com>
2022-10-19 12:40:46 +0200

    Update use-interval.js

--- use-interval.js
+++ use-interval.js
@@ -1,30 +1,28 @@
-import { useEffect, useRef, useState } from "react";
+import {useEffect, useRef, useState} from 'react'
 
 function useInterval(callback, delay, limit) {
-  const [count, setCount] = useState(1);
-  const intervalRef = useRef();
-  const callbackRef = useRef(callback);
+    const [count, setCount] = useState(1)
+    const intervalRef = useRef()
+    const callbackRef = useRef(callback)
 
-  useEffect(() => {
-    callbackRef.current = callback;
-  }, [callback]);
+    useEffect(() => {
+        callbackRef.current = callback
+    }, [callback]);
 
-  useEffect(() => {
-    if (
-      (typeof delay === "number" && count <= limit) ||
-      (typeof delay === "number" && !limit)
-    ) {
-      intervalRef.current = window.setInterval(() => {
-        callbackRef.current(count);
-        setCount((c) => c + 1);
-      }, delay);
-      return () => {
-        window.clearInterval(intervalRef.current);
-      };
-    }
-  }, [delay, count]);
+    useEffect(() => {
+        console.log(delay, count, limit)
+        if ((typeof delay === 'number' && count <= limit) || (typeof delay === 'number' && !limit)) {
+            intervalRef.current = window.setInterval(() => {
+                setCount(c => c + 1)
+                callbackRef.current(count, setCount)
+            }, delay)
+            return () => {
+                window.clearInterval(intervalRef.current)
+            }
+        }
+    }, [delay, count]);
 
-  return intervalRef;
+    return intervalRef;
 }
 
-export default useInterval;
+export default useInterval
